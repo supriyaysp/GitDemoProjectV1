e1a4fa8b29295eedd28b67fadca2305669f78763
syamjala <syamjala@LIN66013010.corp.capgemini.com>
2022-03-15 15:55:42 +0530

    Test3 commit

---
+++ src/Test3.java
@@ -0,0 +1,15 @@
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry="s";
+		if(hungry == "yes"||hungry == "Y" || hungry=="y"||hungry =="s") {
+			System.out.println("eat something...");
+		}
+		else {
+			System.out.println("Do work...");
+		}
+	}
+
+	}
